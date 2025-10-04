7623db7235e66b89598a5a1b9c6fce39d5a479b6
tessmadison <tessxmadison@gmail.com>
2025-10-03 22:06:50 -0400

    retrieve method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,7 +88,21 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
+        // DONE implement this method
+        if (path.isEmpty()) {
+            return getRoot();
+        }
+
+        else {
+            char current = path.charAt(0);
+            if (current == 'l' && left != null) {
+                return left.retrieve(path.substring(1));
+            }
+            else if (current == 'r' && right != null) {
+                return right.retrieve(path.substring(1));
+            }
+        }
+
         return -1;
     }
 
